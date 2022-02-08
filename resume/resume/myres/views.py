e2b5d4d6e2df7453852fef3fa17ac703c3b2dfc7
Suleimanov Artur <assuleimanoff2017@gmail.com>
2022-02-08 17:52:37 +0300

    myres app was finaly installed, base.html template was added, myres/urls.py was added, 404exception view was added

--- resume/resume/myres/views.py
+++ resume/resume/myres/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("myres app's page")
+    return render(request, "myres/base.html", {'title':"Главная страница"})
+
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1 style="text-align: center; color: blue; font-family: sans-serif;">Page not found</h1>')
