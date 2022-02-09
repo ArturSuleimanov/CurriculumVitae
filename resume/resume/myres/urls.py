682ab07146d4a8fa12f7ce352fa849113b4468d0
Suleimanov Artur <assuleimanoff2017@gmail.com>
2022-02-09 21:14:48 +0300

    base template was upgraded, autentication and registration templates were added

--- resume/resume/myres/urls.py
+++ resume/resume/myres/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path("", index, name = 'home'),
+    path("/register", Registration, name = 'registration' ),
+    path("/auth", autentification, name = "autentification")
 ]
--- resume/resume/myres/views.py
+++ resume/resume/myres/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+
 def index(request):
     return render(request, "myres/base.html", {'title':"Главная страница"})
 
@@ -9,3 +13,12 @@ def index(request):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1 style="text-align: center; color: blue; font-family: sans-serif;">Page not found</h1>')
+
+
+
+def Registration(request):
+    return render(request, "myres/registration.html", {'title':'Registration'})
+
+
+def autentification(request):
+    return render(request, "myres/autentification.html", {'title':'autentification'})
